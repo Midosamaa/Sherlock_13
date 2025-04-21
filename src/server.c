be8ab035296c0f01eefec40ad1ad850118212fd2
Midosamaa <ahmedfilali010@gmail.com>
2025-04-21 14:20:03 +0200

    regle le probleme du melange de cartes et ajoute la fonctionnalite d'ecriture sur la grille

--- src/server.c
+++ src/server.c
@@ -5,7 +5,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <time.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 
@@ -230,6 +230,9 @@ int main(int argc, char *argv[])
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
+
+	 srand(time(NULL));
+
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
@@ -396,6 +399,8 @@ int main(int argc, char *argv[])
 			
 				int quantite = tableCartes[cible][symbole];
 			
+				sprintf(reply, "V %d %d %d", cible, symbole, quantite);
+				sendMessageToClient(tcpClients[joueur].ipAddress, tcpClients[joueur].port, reply);
 				// Envoyer la réponse uniquement au joueur demandeur
 				sprintf(reply, "R %d", quantite);
 				sendMessageToClient(tcpClients[joueur].ipAddress, tcpClients[joueur].port, reply);
